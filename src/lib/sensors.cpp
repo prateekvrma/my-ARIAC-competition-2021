162a40b070de42a8eae7f8ed1de58c7f45cd485b
Chang-Hong Chen <longhongc@gmail.com>
2022-03-11 12:15:43 -0500

    Add tranform of parts to world frame

--- src/lib/sensors.cpp
+++ src/lib/sensors.cpp
@@ -1,5 +1,13 @@
 #include "sensors.h"
 
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h> //--needed for tf2::Matrix3x3
+
+#include <nist_gear/Model.h>
+
 Sensors::Sensors(ros::NodeHandle* nodehandle, const std::string &id): 
   m_nh{*nodehandle}, 
   m_id{id} {}
@@ -14,10 +22,54 @@ LogicalCamera::LogicalCamera(ros::NodeHandle* nodehandle, const std::string& id)
 
 void LogicalCamera::sensor_callback(const nist_gear::LogicalCameraImage::ConstPtr& msg)
 {
-  //std::cout << m_id << std::endl; 
-  ROS_INFO_THROTTLE(1, "Callback triggered for Topic %s", m_id.c_str());
-  //ROS_INFO_THROTTLE(3, "%s: %d objects.", m_id.c_str(), (int)msg->models.size());
-  //ROS_INFO("%s: %d objects.", m_id.c_str(), (int)msg->models.size()); 
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+
+  ros::Duration timeout(5.0);
+  geometry_msgs::TransformStamped transformStamped;
+  try {
+    ROS_INFO("%s", m_id.c_str()); 
+    transformStamped = tfBuffer.lookupTransform("world", m_id + "_frame",
+                                                 ros::Time(0), timeout);
+  }
+  catch (tf2::TransformException& ex) {
+    ROS_WARN("%s", ex.what());
+    ros::Duration(1.0).sleep();
+    return; 
+  }
+
+  tf2::Quaternion camera_q_tf(
+    transformStamped.transform.rotation.x,
+    transformStamped.transform.rotation.y,
+    transformStamped.transform.rotation.z,
+    transformStamped.transform.rotation.w);
+
+  for (auto &model: msg->models) {
+    geometry_msgs::Pose model_pose = model.pose; 
+    geometry_msgs::Pose transformed_model_pose; 
+
+    tf2::doTransform(model_pose, transformed_model_pose, transformStamped); 
+
+    // calculate orientation in world frame
+    tf2::Quaternion model_q_tf;
+    tf2::convert(transformed_model_pose.orientation, model_q_tf); 
+    model_q_tf.normalize(); 
+
+    //convert Quaternion to Euler angles
+    tf2::Matrix3x3 m(model_q_tf);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+
+    ROS_INFO("%s in /world frame: [%f,%f,%f] [%f,%f,%f]",
+      model.type.c_str(), 
+      transformed_model_pose.position.x,
+      transformed_model_pose.position.y,
+      transformed_model_pose.position.z,
+      roll,
+      pitch,
+      yaw);
+    }
+
 }
 
 DepthCamera::DepthCamera(ros::NodeHandle* nodehandle, const std::string& id):
