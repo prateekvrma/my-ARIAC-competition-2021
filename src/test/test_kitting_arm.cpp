1c2a1b43cbfeaf23e865d7c04d7e9f6bfbd5c1b4
Chang-Hong Chen <longhongc@gmail.com>
2022-04-13 12:00:27 -0400

    Update test_kitting_arm

--- src/test/test_kitting_arm.cpp
+++ src/test/test_kitting_arm.cpp
@@ -10,43 +10,31 @@ int main(int argc, char** argv) {
     KittingArm kitting_arm = KittingArm();
     // kitting_arm.print_joints_position(); 
 
-    while (ros::ok()) {
-      auto success = kitting_arm.get_order(); 
+    geometry_msgs::Pose part_init_pose; 
+    part_init_pose.position.x = -1.799; 
+    part_init_pose.position.y = 3.48; 
+    part_init_pose.position.z = 0.78; 
 
-      if (success) {
-        kitting_arm.plan(); 
-        kitting_arm.execute(); 
-
-      }
-      else
-        ros::shutdown(); 
-    }
-   
-    // geometry_msgs::Pose part_init_pose; 
-    // part_init_pose.position.x = -1.799; 
-    // part_init_pose.position.y = 3.48; 
-    // part_init_pose.position.z = 0.78; 
-    //
-    // geometry_msgs::Pose part_goal_pose_in_frame; 
-    // part_goal_pose_in_frame.position.x = 0.1; 
-    // part_goal_pose_in_frame.position.y = 0.1; 
-    // part_goal_pose_in_frame.position.z = 0; 
+    geometry_msgs::Pose part_goal_pose_in_frame; 
+    part_goal_pose_in_frame.position.x = 0.1; 
+    part_goal_pose_in_frame.position.y = 0.1; 
+    part_goal_pose_in_frame.position.z = 0; 
     // auto flat_orientation = motioncontrol::quaternionFromEuler(0, 0, M_PI/4);
     // part_goal_pose_in_frame.orientation.x = flat_orientation.getX(); 
     // part_goal_pose_in_frame.orientation.y = flat_orientation.getY(); 
     // part_goal_pose_in_frame.orientation.z = flat_orientation.getZ(); 
     // part_goal_pose_in_frame.orientation.w = flat_orientation.getW(); 
     //
-    // // kitting_arm.pickPart("pump", part_init_pose); 
+    kitting_arm.pickPart("pump", part_init_pose); 
     // kitting_arm.placePart(part_init_pose, part_goal_pose_in_frame, "agv2"); 
 
     // ROS_INFO("Moving to agv1"); 
     // kitting_arm.goToPresetLocation("agv1"); 
     // kitting_arm.print_joints_position(); 
     //
-    // ROS_INFO("Moving to agv2"); 
-    // kitting_arm.goToPresetLocation("agv2"); 
-    // kitting_arm.print_joints_position(); 
+    ROS_INFO("Moving to agv2"); 
+    kitting_arm.goToPresetLocation("agv2"); 
+    kitting_arm.print_joints_position(); 
     //
     // ROS_INFO("Moving to agv3"); 
     // kitting_arm.goToPresetLocation("agv3"); 
